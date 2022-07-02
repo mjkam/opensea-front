cfd0aab5bce0f0f21576a43930a1109e412bbf52
mjkam <mjkam@naver.com>
2022-07-02 15:37:07 +0900

    Header 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
+    <Header />
     <router-view />
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
 
 export default {
-    
+    components: { Header }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="">
-    
+  <div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home'
+  
 }
 </script>
 
